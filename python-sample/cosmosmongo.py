089e995f84c9dc9a3be1adb99ed469bcc0bc4e9d
Wriju Ghosh <wriju_ghosh@hotmail.com>
2021-03-02 17:09:02 +0530

    Update cosmosmongo.py

--- python-sample/cosmosmongo.py
+++ python-sample/cosmosmongo.py
@@ -1,9 +1,9 @@
 # import pymongo
 
 #CosmosDB
-mongoUrl = "mongodb://cosmosfreewg:6gUvMGLaGRvkgysbC6XnFZlBbvnqQxguWBlTDtZZcXtlkawL96Q0njCNGzDcwU1wW9L1QAdwszKzUVLxc9AT5g==@cosmosfreewg.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosfreewg@"
+mongoUrl = ""
 
 # MongoDB Atlas 
-mongoAtlasUrl = "mongodb+srv://wriju:P%40ssw0rd@cluster0.xkcmq.mongodb.net/hrdb?retryWrites=true&w=majority"
+mongoAtlasUrl = ""
 
 # mongodb+srv://wriju:"P@ssw0rd"@cluster0.xkcmq.mongodb.net/hrdb?retryWrites=true&w=majority
